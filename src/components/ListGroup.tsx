7bedb7c5daefe0099de895369ef3cc27d759e965
GiacomoPracucci <giacomo.pracucci8@gmail.com>
2024-12-17 22:19:23 +0100

    ci riprovo

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,17 @@
 function ListGroup() {
     return (
-        <ul className="list-group">
-            <li className="list-group-item">An item</li>
-            <li className="list-group-item">A second item</li>
-            <li className="list-group-item">A third item</li>
-            <li className="list-group-item">A fourth item</li>
-            <li className="list-group-item">And a fifth one</li>
-        </ul>
+        // con react ogni componente può restituire un solo elemento
+        // se vogliamo mettere un Header a questa lista dobbiamo inserire tutto in un div
+        <div>
+            <h1>List</h1>
+            <ul className="list-group">
+                <li className="list-group-item">An item</li>
+                <li className="list-group-item">A second item</li>
+                <li className="list-group-item">A third item</li>
+                <li className="list-group-item">A fourth item</li>
+                <li className="list-group-item">And a fifth one</li>
+            </ul>
+        </div>
     );
 }
 
